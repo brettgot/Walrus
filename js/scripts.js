32c7880c96894bfab63a5b291ac7b48963dba208
Student <example@example.com>
2019-11-13 20:17:55 -0800

    created buttons to change container color

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
   $("h1").click(function() {
     $("p").slideToggle();
     $("img").slideToggle();
-    
+
+  });
+  $("button#lightblue").click(function() {
+    $("#box").removeClass();
+    $("#box").addClass("lightblue-background");
+  });
+  $("button#lightgreen").click(function() {
+    $("#box").removeClass();
+    $("#box").addClass("lightgreen-background");
   });
 });
